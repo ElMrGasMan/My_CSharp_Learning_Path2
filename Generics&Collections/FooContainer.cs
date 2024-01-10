af35dd6a4c90fdd7a3f2b5e25066be1ad589333e
Agustin Schipani <agus-calderon-schip03@outlook.com>
2024-01-09 22:01:11 -0300

    Added about Generic constraints.

---
+++ Generics&Collections/FooContainer.cs
@@ -0,0 +1,73 @@
+﻿namespace Generics_Collections
+{
+    internal class FooContainer<T> where T : Foo
+    {
+        private T[] values;
+        private int count = 0;
+
+        public int GetLength() => values.Length;
+
+        public FooContainer(int maxSize)
+        {
+            values = new T[maxSize];
+        }
+
+        public void AddValue(T value) 
+        {
+            values[count++] = value;
+        }
+
+        public void InsertValue(int index, T value)
+        {
+            if (index < 0 || index >= values.Length) throw new IndexOutOfRangeException();
+            else values[index] = value;
+        }
+
+        public T ObtainValue(int index) => values[index];
+        public T ObtainValue() => values[count-1];
+
+    }
+
+    class Foo 
+    {
+        private int magicNumber;
+
+        public int MagicNumber { get => magicNumber; set => magicNumber = value; }
+
+        public Foo(int magicNumber)
+        {
+            this.magicNumber = magicNumber;
+        }
+
+        public override string? ToString()
+        {
+            return $"The magic number is... {magicNumber}.";
+        }
+    }
+
+    class Foos : Foo
+    {
+        public Foos(int magicNumber) : base(magicNumber)
+        {
+        }
+    }
+
+    class Foouk : Foo
+    {
+        public Foouk(int magicNumber) : base(magicNumber)
+        {
+        }
+    }
+
+    class Doos 
+    {
+        private int normalNumber;
+
+        public int NormalNumber { get => normalNumber; set => normalNumber = value; }
+
+        public Doos(int normalNumber)
+        {
+            this.normalNumber = normalNumber;
+        }
+    }
+}
--- Generics&Collections/Program.cs
+++ Generics&Collections/Program.cs
@@ -4,19 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Container<string> stringContainer = new(3);
+            FooContainer<Foo> stringContainer = new(3);
 
-            stringContainer.AddValue("Bismarck class");
-            stringContainer.AddValue("Yamato class");
-            stringContainer.AddValue("North Carolina class");
+            stringContainer.AddValue(new Foo(25));
+            stringContainer.AddValue(new Foos(45));
+            stringContainer.AddValue(new Foouk(69));
+            //stringContainer.AddValue(new Doos(67)); <--- Not possible since Doos does not inherit Foo
 
-            Console.WriteLine("My 3 favorite classes of battleships are: ");
+            Console.WriteLine("The three foo classes are: ");
 
             for (int i = 0; i < stringContainer.GetLength(); i++)
             {
                 Console.WriteLine(stringContainer.ObtainValue(i) + ".");
             }
-            
+
+            Console.WriteLine("The last value of the container is: ");
+            Console.WriteLine(stringContainer.ObtainValue() + ".");
+
         }
     }
 }
